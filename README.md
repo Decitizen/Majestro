29ee115fa308d83e50358644b6fc5408a3a2e520
Ville Saarinen <ville.saarinen@helsinki.fi>
2018-03-09 02:41:59 +0200

    0.5 making majestro public

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Majestro
-Majestro Password Manager
+__Majestro Password Manager__ is a light Chrome extension that helps you to remember your passwords. 
+
+Majestro's key derivation function is dependent on two secret keys: 
+
+1. smart number 
+2. master password 
+
+Majestro uses sha256 and PBKDF2 (Password-Based Key Derivation Function 2) for password derivation, and uses [__Stanford Javascript Crypto Library__](https://github.com/bitwiseshiftleft/sjcl)
+
+#### Test user:
+__Smart number:__ 123456
+__Master Password:__ this is the master password 
---
+++ majestro_chrome_extension/script/popup.js
@@ -0,0 +1,481 @@
+/** @fileOverview Majestro PasswordManager's GUI implementation.
+ *
+ * @author Ville Saarinen
+ */
+
+/**
+ * Adds suitable eventlisteners and defines actions for GUI transitions.
+ */
+const USERS_SITES_FILENAME   = 'list_of_crys.pswx'; 
+const USER_ACCOUNTS_FILENAME = 'user_accounts.json';
+
+document.addEventListener('DOMContentLoaded', function() {
+  
+  // Buttons
+  var smart_submit_button = document.getElementById('smart_submit_button');
+  var add_account_button = document.getElementById('add_account_button');
+  var id_submit_button = document.getElementById('id_submit_button');
+  var copy_button = document.getElementById('copy_button');
+  var cancel_add_account = document.getElementById('cancel_add_account_button');
+  var submit_new_account_button = document.getElementById('submit_new_account_button');
+  var import_accounts_button = document.getElementById('import_accounts_button');
+  
+  // Inputs
+  var import_accounts_file_input = document.getElementById('import_accounts_file_input');
+  var mpassword_input = document.getElementById('mpassword_input');
+  var copy_input = document.getElementById('copy_input');
+  save_current_url();
+
+  smart_submit_button.addEventListener('click', function() {
+    load_from_storage('current_url', (current) => {
+      console.log('Loaded current url successfully: ', current);
+
+      let site_datalist = document.getElementById('site_datalist');
+      let add_account_input = document.getElementById('add_account_input');
+      
+      if (validate_smart_number()) {
+        console.log("Unlock with smart number performed successfully.");
+        
+        let site_array = read_user_accounts(USERS_SITES_FILENAME);
+        let recognized = is_site_recognized(current, site_array);
+        
+        site_datalist.value = recognized;
+        add_account_input.value = current;
+
+        populate_site_list(site_array);
+        transition_to_id_selection();
+      }
+    });
+  });
+
+  id_submit_button.addEventListener('click', function() {
+
+    let site_array    = read_user_accounts(USERS_SITES_FILENAME);
+    let site_datalist = document.getElementById('site_datalist');
+
+    if (is_site_recognized(site_datalist.value, site_array)) {
+      $('#site_selector_panel').fadeOut('500', function() {
+        $('#masterpw_panel').fadeIn('500');
+        $('#site_select_error_message').hide();
+      });  
+    } else {
+      document.getElementById('site_select_error_message').innerHTML = "• Account doesn't exist.";
+      $('#site_select_error_message').show();
+    }
+    
+  });
+
+  copy_button.addEventListener('click', function() {
+    var mpassword = document.getElementById('copy_input');
+    copy_to_clipboard(mpassword.value);
+  });
+
+  add_account_button.addEventListener('click', function() {
+    $('#site_selector_panel').fadeOut('900', function() {
+      $('#add_account_panel').fadeIn('900');
+    });
+    
+  });
+
+  cancel_add_account.addEventListener('click', function() {
+    $('#add_account_panel').fadeOut('900', function() {
+      $('#site_selector_panel').fadeIn('900');
+    });
+  });
+
+  submit_new_account_button.addEventListener('click', function() {
+    $('#add_account_panel').fadeOut('900', function() {
+      add_new_account();
+      $('#site_selector_panel').fadeIn('900');
+    });
+  });
+
+  import_accounts_button.addEventListener('click', function() {
+    $('#import_export_panel').fadeOut('900', function() {
+      $('#import_panel').fadeIn('900');
+    });
+  });
+
+   copy_input.addEventListener('mouseover', function() {
+    copy_input.type = "text";
+  });
+
+  copy_input.addEventListener('mouseout', function() {
+    copy_input.type = "password";
+  });
+
+  import_accounts_file_input.addEventListener('onchange', function() {
+    
+  });
+
+  mpassword_input.addEventListener('keyup', function() {
+    var input_number = document.getElementById('smart_number_input');
+    let mpassword_check_sign = document.getElementById('mp_check_sign');
+
+    if (validate_masterpw_hash( mpassword_input.value, 
+                                input_number.value, 
+                                USER_ACCOUNTS_FILENAME )) {
+
+      mpassword_check_sign.style.color = '#65aa05';
+      mpassword_check_sign.innerHTML = '';
+      $('#masterpw_input_error').fadeOut('fast');
+
+      derive_password( mpassword_input.value, 
+                        input_number.value, 
+                        USER_ACCOUNTS_FILENAME );
+    } else {
+      mpassword_check_sign.style.color = '#ff9c2b';
+      mpassword_check_sign.innerHTML = '';
+    }
+  });
+
+  $('.site_item').hover(function () {
+    $(this).toggleClass('site_item_hover');
+  });
+ 
+
+});
+
+/**
+ * Check if a match for current site is found in the list.
+ * @param {String} current - name of the current tab as a string
+ * @param {Array} site_array - site_array - sites as an array of strings
+ * @return {String} recognized - String if recognized, otherwise null 
+ */
+function is_site_recognized(current, site_array) {
+  recognized = null;
+  for (let site of site_array) {
+    if (site.toLowerCase().includes(current)) {
+      recognized = site;
+    }
+  }
+
+  let message = recognized ? "Site recognized." : "Couldn't recognize site.";
+  console.log(message);
+
+  return recognized; 
+}
+
+/**
+ * Handles keyevents for ENTER, offered as an alternative way to confirm input.
+ */
+document.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  var smart_number_panel = document.getElementById('smart_number_panel');
+  var site_selector_panel = document.getElementById('site_selector_panel');
+  var masterpw_panel = document.getElementById('masterpw_panel');
+
+  if (event.keyCode === 13) {
+    if (smart_number_panel.style.display != 'none') {
+     
+     // 1. Case, input smart number
+      var smart_submit_button = document.getElementById('smart_submit_button');
+      smart_submit_button.click();
+
+    } else if (site_selector_panel.style.display != 'none') {
+      
+      // 2. Case, choose correct site from datalist
+      var id_submit_button = document.getElementById('id_submit_button');
+      id_submit_button.click();
+
+    }
+  }
+});
+
+/**
+ * Handles loading of user's account details from json file.
+ * @param {String} USER_ACCOUNTS_FILENAME
+ * @return {Object} - includes content's of the json-file as key-value pairs
+ */
+function load_user_account(USER_ACCOUNTS_FILENAME) {
+  var user_acc_url = chrome.runtime.getURL(USER_ACCOUNTS_FILENAME);
+  var file_content = load_json(user_acc_url);
+  
+  return JSON.parse(file_content);
+}
+
+/**
+ * Validates that hash of the master password is valid.
+ * @param {String} mpassword - master password associated with account
+ * @param {String} smart_number - smart number associated with account
+ * @param {String} USER_ACCOUNTS_FILENAME
+ */
+function validate_masterpw_hash(mpassword, smart_number, USER_ACCOUNTS_FILENAME) {
+  const CYCLES = 12000;
+  const PW_LENGTH = 256;
+
+  // Create a PBKDF2 hash using master password and smart number
+  let mpassword_hash = sjcl.hash.sha256.hash(mpassword);
+      mpassword_hash = sjcl.codec.hex.fromBits(mpassword_hash);
+  
+  var sm_number_hash = sjcl.hash.sha256.hash(smart_number);
+  var derived_pbkdf2 = sjcl.misc.pbkdf2(mpassword_hash, sm_number_hash, CYCLES, PW_LENGTH);
+      derived_pbkdf2 = sjcl.codec.hex.fromBits(derived_pbkdf2); 
+  
+  // Verify with original hash
+  user_acc_details = load_user_account(USER_ACCOUNTS_FILENAME);
+  if (user_acc_details.masterpw == derived_pbkdf2) {
+    console.log('Masterpw validated.');
+
+    return true;
+  }
+  console.log('Masterpw invalid.');
+  return false;
+}
+
+/**
+ * Handles derivation of site-specific passwords using master password,
+ * smart number and account name.
+ * @param {String} mpassword - master password associated with account
+ * @param {String} smart_number - smart number associated with account
+ * @param {String} USER_ACCOUNTS_FILENAME
+ * Uses external library: cry_trans1.js
+ */
+function derive_password(mpassword, smart_number, USER_ACCOUNTS_FILENAME) {
+  const CYCLES = 10000;
+  const PW_LENGTH = 256;
+  const MOD_VALUE = 17;
+
+  var site_name = document.getElementById('site_datalist');
+  $('#smart_number_error').hide();
+  $('#masterpw_panel').fadeOut('slow', function() {
+    // Site-specific password is derived in 4 phases
+
+    // 1) Concatenate smart number + master password + account name, 
+    //    and hash resulting string by sha256 
+    var pw_hash    = sjcl.hash.sha256.hash(smart_number + mpassword + site_name.value);
+    
+    // 2) Concatenate smart number with account name, 
+    //    and hash resulting string by sha256 
+    var smart_hash = sjcl.hash.sha256.hash(smart_number + site_name.value);
+        smart_hash = sjcl.codec.hex.fromBits(smart_hash); 
+    
+    // 3) Using first hash as a password and second as a salt, derive the final hash
+    //    using PBKDF2 with more approriate parameters, format into base64 
+    var derived_pw = sjcl.misc.pbkdf2(pw_hash, smart_hash, CYCLES, PW_LENGTH);
+    var password   = sjcl.codec.base64.fromBits(derived_pw);
+
+    // 4) Select a 24 letter substring of the string based on smart number
+    //    modded with constant value 17
+    begin_pw_placement = smart_number % 17
+
+    password = password.substring(begin_pw_placement, begin_pw_placement+24);
+    console.log('Derived Password:', password);
+
+    var copy_input = document.getElementById('copy_input');
+    copy_input.value = password;
+
+    $('#copy_panel').fadeIn('slow');
+  });
+}
+
+/**
+ * Handle functionality for adding new account.
+ */
+function add_new_account() {
+  var add_account_panel = document.getElementById('add_account_panel');
+  save_account('');
+  $('#add_account_panel').fadeIn('500');
+}
+
+/**
+ * Enables copying of given string to clipboard
+ * @param {String} text_to_copy 
+ */
+function copy_to_clipboard(text_to_copy) {
+
+  const input = document.createElement('input');
+  input.style.position = 'fixed';
+  input.style.opacity = 0;
+  input.value = text_to_copy;
+  
+  document.body.appendChild(input);
+  input.select();
+  document.execCommand('Copy');
+  document.body.removeChild(input);
+}
+
+/**
+ * Fetches the list of sites associated with user's account using XMLHttpRequest.
+ * @param {String} filename
+ * @return {Array} site_array - sites as an array of strings
+ */
+function read_user_accounts(filename) {
+  var filePath = chrome.runtime.getURL(filename);
+  
+  xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET",filePath,false);
+  xmlhttp.send();
+
+  var fileContent = xmlhttp.responseText;
+  var site_array = fileContent.split('\n');
+
+  return site_array;
+}
+
+/**
+ * Populates the datalist with sites given as a parameter.
+ * @param {Array} site_array - sites as an array of strings
+ */
+function populate_site_list(site_array) {
+  var options = '';
+  for(var i = 0; i < site_array.length; i++) {
+    options += '<option value="'+site_array[i]+'" />';
+  }
+  document.getElementById('select_site').innerHTML = options;
+}
+
+/**
+ * Helper routine for transforming the logo/icon from the landing view to 
+ * the next.
+ */
+function transform_icon() {
+  var icon_container       = document.getElementById('icon_container');
+  var icon_container_small = document.getElementById('icon_container_small');
+
+  icon_container.style.display       = 'none';
+  icon_container_small.style.display = '';
+}
+
+/**
+ * Validates the form of smart number input.
+ * @return {Boolean} - true signifies successful validation
+ */
+function validate_smart_number() {
+  var smart_number  = document.getElementById('smart_number_input');
+  var error_message = document.getElementById("smart_number_error");
+
+  if (isNaN(smart_number.value)) {
+    if (error_message.style.display === 'none') {
+      error_message.innerHTML = "Not a number, try again.";
+      error_message.style.display = '';
+    }
+    return false;
+
+  } else if (!validate_smart_hash(smart_number.value, USER_ACCOUNTS_FILENAME)){
+    if (error_message.style.display === 'none') {
+      error_message.style.display = '';
+    }
+    error_message.innerHTML = "Invalid Smart number.";
+    return false;
+
+  } else {
+    if (error_message.style.display !== 'none') {
+      error_message.style.display = 'none';
+    }  
+
+    return true; 
+  }
+}
+
+/**
+ * Handle JQuery transitions from landing page (smart number input) to the 
+ * actual app.
+ */
+function transition_to_id_selection() {
+  $('body').fadeOut('fast', function() {
+      // Smooth transition
+      transform_icon();
+      $("#smart_number_panel").hide();
+      $("#site_selector_panel").show();
+      $('body').fadeIn('500');
+    });
+}
+
+/**
+ * Load json from given url with XMLHttpRequest (synchronous call)
+ * @param {String} json_url - filename
+ * @return {response}
+ */
+function load_json(json_filename) {
+  var xmlhttp = new XMLHttpRequest();
+  
+  xmlhttp.open("GET",json_filename,false);
+  xmlhttp.send();
+
+  return xmlhttp.responseText;
+}
+
+/**
+ * Validate smart number input by comparing sha256 hash of input with the ones 
+ * fetched from the user_accounts.json
+ * @param  {Number}  smart_input_value 
+ * @param  {String}  USER_ACCOUNTS_FILENAME - filename as a string
+ * @return {Boolean} 
+ */
+function validate_smart_hash(smart_input_value, USER_ACCOUNTS_FILENAME) {
+  var user_acc_url = chrome.runtime.getURL(USER_ACCOUNTS_FILENAME);
+  var file_content = load_json(user_acc_url);
+  
+  var accounts_json   = JSON.parse(file_content);
+  var orig_smart_hash = accounts_json.smartnum;
+  var input_hash      = sjcl.hash.sha256.hash(smart_input_value);
+  input_hash          = sjcl.codec.hex.fromBits(input_hash);
+
+  if (orig_smart_hash == input_hash) {
+    console.log("Smart number verified: ");
+    console.log("User: " + accounts_json.user);
+      
+    return true;
+  }
+
+  console.log("Smart number didn't match with a known user.");
+  return false;
+}
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback called when the URL of the current tab
+ *   is found.
+ */
+function get_current_url(callback) {
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, (tabs) => {
+    var tab = tabs[0];
+    var url = tab.url;
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+    callback(url);
+  });
+}
+
+/**
+ * Save current URL to the storage API.
+ */
+function save_current_url() {
+  get_current_url((url) => {
+      console.log('Url fetched:', url);
+
+      strip_url = url.replace(/(^\w+:|^)\/\//, '');
+      url_array = strip_url.split(".");
+      
+      for (var i = 0; i < url_array.length; i++) {
+        if (url_array[i] != 'www') {
+          break;
+        }
+      }
+
+      var items = {};
+      items['current_url'] = url_array[i];
+      chrome.storage.sync.set(items);
+
+      console.log('Url saved successfully.');
+  });
+}
+
+/**
+ * Load current URL by using the storage API.
+ * @param {String} item - name of the item (key in the hashtable)
+ * @param {function(string)} callback called when the actual URL of 
+ * the current tab is found from the storage API.
+ */
+function load_from_storage(item, callback) {
+  chrome.storage.sync.get(item, (items) => {
+    console.log('Loading item ', item, '...');
+    callback(chrome.runtime.lastError ? null : items[item]);
+  });
+}
